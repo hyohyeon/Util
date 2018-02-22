dff49c27b6fb68c7667ee4fef336a97ece252667
hyohyeon <aha05002@naver.com>
2018-02-22 17:41:31 +0900

    반올림, 내림, comma, 공백

--- UnitTest/src/com/test/string/SubstringTest.java
+++ UnitTest/src/com/test/string/SubstringTest.java
@@ -20,7 +20,7 @@ class SubstringTest {
 		String test = "";
 		Boolean result3 = util.valueCheck(test);
 		System.out.println(result3);*/
-		Boolean result = util.dateCompare("20170102", "20170101");
+/*		Boolean result = util.dateCompare("20170102", "20170101");
 		System.out.println(result); 
 		Date date = util.stringToDate("17/01/02");
 		System.out.println(date);
@@ -30,8 +30,20 @@ class SubstringTest {
 		System.out.println(resultDate);
 		String day = util.getDateDay("20180222","yyyyMMdd");
 		System.out.println(day);
-		
 		System.out.println(resultDate+"는 "+day+"요일입니다.");
+		
+		String removeAllSpace = util.removeAllSpace("아녕                  나 는 효현이");
+		System.out.println(removeAllSpace);*/
+/*		String date1 = util.substringIndex("20180222", 1, 6);
+		System.out.println();
+		String date2 = util.calculationOfDate(date1, 30, "yyyyMM");
+		System.out.println(date2);*/
+
+		String value = util.addComma("30000000000000");
+		System.out.println(value);
+		String roundroundAround = util.roundUp("300.11231231231231231233");
+		System.out.println(roundroundAround);
+		
 		
 	}
 
--- UnitTest/src/com/test/string/Util.java
+++ UnitTest/src/com/test/string/Util.java
@@ -1,5 +1,7 @@
 package com.test.string;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -94,15 +96,22 @@ public class Util {
 		SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyyy-mm-dd");
 		SimpleDateFormat dateFormat2 = new SimpleDateFormat("yyyy/mm/dd");
 		SimpleDateFormat resultformat = new SimpleDateFormat("yyyymmdd");
-		Date result;
+		Date result, errorResult = null;
+		String errorDate = "00000000";
+
+		try {
+			errorResult = resultformat.parse(errorDate);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
 
 		if (date.contains("-")) {
 			try {
 				result = dateFormat1.parse(date);
 				return result;
 			} catch (ParseException e) {
+				return errorResult;
 
-				e.printStackTrace();
 			}
 
 		}
@@ -112,7 +121,7 @@ public class Util {
 				return result;
 			} catch (ParseException e) {
 
-				e.printStackTrace();
+				return errorResult;
 			}
 
 		}
@@ -122,9 +131,8 @@ public class Util {
 			return result;
 		} catch (ParseException e) {
 
-			e.printStackTrace();
+			return errorResult;
 		}
-		return null;
 
 	}
 
@@ -138,7 +146,9 @@ public class Util {
 	 * @since : 2018-02-22
 	 */
 	public String calculationOfDate(String date, int when, String type) {
-
+		if (date == null) {
+			date = "";
+		}
 		Date defaultTypeDate = stringToDate(date);
 
 		Calendar c = Calendar.getInstance();
@@ -201,7 +211,68 @@ public class Util {
 		}
 		return day;
 	}
+
+	/**
+	 * @MethodName : 모든공백 지우기(좌우,중간 모두)
+	 * @param String
+	 * @author : 박효현
+	 * @since : 2018-02-22
+	 */
+	public String removeAllSpace(String data) {
+		data = data.replaceAll("\\p{Z}", "");
+		return data;
+	}
 	
+	/**
+	 * @MethodName : 반올림, 내림 함수(round)
+	 * @param String
+	 * @author : 박효현
+	 * @since : 2018-02-22
+	 */
+	public String roundUp(String s){
+		if(s != null && isNumeric(s.replace(".", ""))){
+			float val = Float.parseFloat(s);
+			int rVal = Math.round(val);
+			return Integer.toString(rVal);
+		}else{
+			return s;
+		}
+	}
+	/**
+	 * @MethodName : String으로 표현된 숫자데이터 숫자 단위별 comma(,) 찍기
+	 * @param String
+	 * @author : 박효현
+	 * @since : 2018-02-22
+	 */
+	public String addComma(String s) {
+		if (s != null && isNumeric(s.replace(".", ""))) {
+			double val = Double.parseDouble(s);
+			NumberFormat formatter = new DecimalFormat("###,###,###.##");
+			return formatter.format(val);
+		} else {
+			return s;
+		}
+	}
+	
+	
+	
+	/**
+	 * @MethodName : String으로 표현된 숫자데이터가 전부 숫자인지 확인하는 함수
+	 * @param String
+	 * @author : 박효현
+	 * @since : 2018-02-22
+	 */
+	public boolean isNumeric(String str) {
+		for (int i = 0, len = str.length(); i < len; ++i) {
+			if (!Character.isDigit(str.charAt(i))) {
+				if (i == 0 && str.charAt(i) == 0x2D) {
+					continue;
+				}
+				return false;
+			}
+		}
+		return true;
+	}
 	
 
 }
