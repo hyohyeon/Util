449e9e596396e635342ecd1f7084a4767b566573
hyohyeon <aha05002@naver.com>
2018-11-29 13:41:17 +0900

    스트링 체크 및 안드로이드 텍스트뷰 색상변환 함수 등.. 추가

--- UnitTest/src/com/test/string/SubstringTest.java
+++ UnitTest/src/com/test/string/SubstringTest.java
@@ -1,6 +1,4 @@
-package com.test.string;
-
-import java.util.Date;
+package com.test.string2;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,43 +7,37 @@ class SubstringTest {
 	@Test
 	void testSubString() {
 		Util util = new Util();
-		/*String result = util.substring("asd", 5);
-		
-		System.out.println(result);
-		
-		String result2 = util.substringIndex("ㅁㅁㅁㅁㅁdddddd", 6, 12);
-		
-		System.out.println(result2);
-		
-		String test = "";
-		Boolean result3 = util.valueCheck(test);
-		System.out.println(result3);*/
-/*		Boolean result = util.dateCompare("20170102", "20170101");
-		System.out.println(result); 
-		Date date = util.stringToDate("17/01/02");
-		System.out.println(date);
-		
-		
-		String resultDate = util.calculationOfDate("20180222", 0, "yyyy/MM/dd");
-		System.out.println(resultDate);
-		String day = util.getDateDay("20180222","yyyyMMdd");
-		System.out.println(day);
-		System.out.println(resultDate+"는 "+day+"요일입니다.");
-		
-		String removeAllSpace = util.removeAllSpace("아녕                  나 는 효현이");
-		System.out.println(removeAllSpace);*/
-/*		String date1 = util.substringIndex("20180222", 1, 6);
-		System.out.println();
-		String date2 = util.calculationOfDate(date1, 30, "yyyyMM");
-		System.out.println(date2);*/
 
-		String value = util.addComma("30000000000000");
-		System.out.println(value);
-		String roundroundAround = util.roundUp("300.11231231231231231233");
-		System.out.println(roundroundAround);
+/*		String result = util.intCompareStringVersion("1 2 3 4");
+		System.out.println(result);*/
+//		int[] array = { 5, 9, 7, 10 };
+//		int[] array = { 4, 9, 7, 1 };
+/*		int x[] = {5, 4, 3};
+		int r = util.sumDivisor(12);
 		
+		System.out.println(r);*/
 		
+/*		String s = util.getMiddle("power");
+		System.out.println(s);
+		*/
+
+/*		int[][] A = { { 1, 2 }, { 2, 3 } };
+		int[][] B = { { 3, 4 }, { 5, 6 } };
+		int[][] answer = util.sumMatrix(A, B);
+		if (answer[0][0] == 4 && answer[0][1] == 6 && 
+				answer[1][0] == 7 && answer[1][1] == 9) {
+			System.out.println("맞았습니다. 제출을 눌러 보세요");
+		} else {
+			System.out.println("틀렸습니다. 수정하는게 좋겠어요");
+		}
+        System.out.println(Arrays.toString(util.gcdlcm(3, 12)));*/
+		//Boolean tf = util.isHarshad(12);
+		//System.out.println(tf);
+		
+//		String result = util.cleanSpecialCharacters("sfsdf123★☆-");
+//		System.out.println(result);
+		System.out.println("123123");
 	}
 
+} 
 
-}
--- UnitTest/src/com/test/string/Util.java
+++ UnitTest/src/com/test/string/Util.java
@@ -1,11 +1,15 @@
-package com.test.string;
+package com.test.string2;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+
+import org.junit.runner.Result;
 
 public class Util {
 
@@ -222,22 +226,23 @@ public class Util {
 		data = data.replaceAll("\\p{Z}", "");
 		return data;
 	}
-	
+
 	/**
 	 * @MethodName : 반올림, 내림 함수(round)
 	 * @param String
 	 * @author : 박효현
 	 * @since : 2018-02-22
 	 */
-	public String roundUp(String s){
-		if(s != null && isNumeric(s.replace(".", ""))){
+	public String roundUp(String s) {
+		if (s != null && isNumeric(s.replace(".", ""))) {
 			float val = Float.parseFloat(s);
 			int rVal = Math.round(val);
 			return Integer.toString(rVal);
-		}else{
+		} else {
 			return s;
 		}
 	}
+
 	/**
 	 * @MethodName : String으로 표현된 숫자데이터 숫자 단위별 comma(,) 찍기
 	 * @param String
@@ -253,9 +258,7 @@ public class Util {
 			return s;
 		}
 	}
-	
-	
-	
+
 	/**
 	 * @MethodName : String으로 표현된 숫자데이터가 전부 숫자인지 확인하는 함수
 	 * @param String
@@ -273,6 +276,243 @@ public class Util {
 		}
 		return true;
 	}
-	
 
+	/**
+	 * @MethodName : 문자열자르기 / split함수
+	 * @param data
+	 *            : string, split : string ex) data : "나는,배가,고프다" / split : ","
+	 * @return String[] 배열
+	 * @author : 박효현
+	 * @since : 2018-02-23
+	 */
+	public String[] split(String data, String split) {
+		String[] returnData = data.split(split);
+		return returnData;
+	}
+
+	/**
+	 * @MethodName : 대->소, 소->대문자 변환
+	 * @param caseIs
+	 *            : 1 : LowerToUpper, 2 : upperToLower
+	 * @author : 박효현
+	 * @since : 2018-02-23
+	 */
+	public String toUpperOrLowerCase(String data, int caseIs) {
+
+		if (caseIs == 1) {
+			return data.toUpperCase();
+		}
+		if (caseIs == 2) {
+			return data.toLowerCase();
+		}
+		return data;
+	}
+
+	/**
+	 * @MethodName : 문자결합 / 문자합치기
+	 * @author : 박효현
+	 * @param :
+	 *            두 파라메터중 한가지 값이 null 이라면 !null 인 값 return 두 파라메터 모두 null 이라면 ""값
+	 *            return
+	 * @since : 2018-02-23
+	 */
+	public String concatString(String data1, String data2) {
+		if (data1 != null && data2 != null) {
+			return data1.concat(data2);
+		} else if (data1 == null) {
+			return data2;
+		} else if (data2 == null) {
+			return data1;
+		}
+		return "";
+	}
+
+	/**
+	 * @MethodName : 최대값 최소값 구하기
+	 * @author : 박효현
+	 * @param :
+	 *            String = 1 2 3 4 5 6
+	 * @return : String = 1 6
+	 * @since : 2018-03-09
+	 */
+
+	public String intCompareStringVersion(String data) {
+		String[] splitData = split(data, "\\s");
+
+		int smaller = 0;
+		int bigger = 0;
+		for (String w : splitData) {
+
+			int num = Integer.parseInt(w);
+			if (smaller == 0 && bigger == 0) {
+				smaller = num;
+				bigger = num;
+			}
+			if (num > bigger) {
+				bigger = num;
+			} else if (num < smaller) {
+				smaller = num;
+			}
+		}
+		String result = String.valueOf(smaller) + " " + String.valueOf(bigger);
+		return result;
+
+	}
+
+	public int getMean(int[] array) {
+		int result = 0;
+		int length = array.length;
+		for (int x : array) {
+			result = result + x;
+		}
+		return result = result / length;
+	}
+
+	public int sumDivisor(int num) {
+		int answer = 0;
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				answer += i;
+			}
+		}
+		return answer;
+	}
+
+	public String printTrangle(int num) {
+		for (int x = 1; x <= num; x++) {
+			for (int y = 0; y < x; y++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		return "";
+	}
+
+	public String getMiddle(String word) {
+		int wordLeng = word.length();
+		int index = wordLeng / 2;
+
+		return (wordLeng % 2 == 0) ? word.substring(index - 1, index + 1) : word.substring(index, index + 1);
+
+	}
+
+	public int[][] sumMatrix(int[][] A, int[][] B) {
+		int[][] answer = new int[A.length][A[0].length];
+
+		for (int x = 0; x < A.length; x++) {
+			for (int y = 0; y < A[x].length; y++) {
+				answer[x][y] = A[x][y] + B[x][y];
+			}
+			;
+		}
+		;
+		return answer;
+
+	}
+
+	public int[] gcdlcm(int a, int b) {
+		int[] answer = new int[2];
+		int temp;
+		if (a > b) {
+			temp = a;
+			b = a;
+			a = temp;
+		}
+		int one, two;
+		for (int i = 1; i <= a; i++) {
+			if (a % i == 0 && b % i == 0) {
+				one = i;
+				two = i * (a / i) * (b / i);
+				answer[0] = one;
+				answer[1] = two;
+			}
+		}
+
+		return answer;
+	}
+
+	public boolean isHarshad(int num) {
+
+		String sNum = Integer.toString(num);
+		String[] arrNum = split(sNum, "");
+		int numLeng = arrNum.length;
+		int[] reArrNum = new int[numLeng];
+		int sum = 0;
+		for (int i = 0; i < numLeng; i++) {
+			reArrNum[i] = Integer.parseInt(arrNum[i]);
+			sum += reArrNum[i];
+		}
+		;
+		if (num % sum == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @MethodName : 텍스트가 존재하는 지 검사하는 함수(null 및 빈스트링 검사)
+	 * @author : 박효현
+	 * @param :
+	 *            String
+	 * @return : boolean
+	 * @since : 2018-11-29
+	 */
+	public static boolean hasText(String str) {
+		boolean resultValue = true;
+
+		if (str == null) {
+			resultValue = false;
+		} else if (str.length() == 0) {
+			resultValue = false;
+		}
+		return resultValue;
+	}
+
+	/**
+	 * 빈스트링 체크 함수
+	 * 
+	 * @author : 박효현
+	 * @param :
+	 *            String
+	 * @return : boolean
+	 * @since : 2018-11-29
+	 */
+	public static boolean isEmptyString(String str) {
+		return !hasText(str);
+	}
+	/**
+	 * 1. ArrayList의 모든 textView는 textColor = black 2. View가 null이 아닐 경우에는 해당
+	 * view(TextView)만 글자 색상 변경
+	 * 
+	 * @author : 박효현
+	 * @param :
+	 *            ArrayList<TextView>
+	 * @since : 2018-11-29
+	 */
+	/*
+	 * 안드로이드에서 사용.. public static void ChangeOnAlignment(ArrayList<TextView> tvArr,
+	 * View v) { for (int i = 0; i < tvArr.size(); i++) {
+	 * tvArr.get(i).setTextColor(Color.BLACK); } // 선택컬럼 색상 변경 if (v != null) {
+	 * TextView selected = null; selected = (TextView) v;
+	 * selected.setTextColor(Color.rgb(0, 122, 255)); } }
+	 */
+
+	/**
+	 * 문자열 내 특수문자 제거 함수
+	 * 
+	 * @author : 박효현
+	 * @param :
+	 *            String
+	 * @return : String
+	 * @since : 2018-11-29
+	 */
+	public static String cleanSpecialCharacters(String str) {
+		String resultValue = "0";
+
+		if (null == str) {
+		} else if (str.matches(".*[0-9].*")) {
+			resultValue = str.replaceAll("[^0-9,.]", "");
+		}
+		return resultValue;
+	}
 }
